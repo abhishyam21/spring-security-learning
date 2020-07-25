c05c5d9bfbc93527dc8cb61ae6e627d41498e86b
300069965 <abhishyam.chennayipalem@myntra.com>
2020-07-25 12:57:21 +0530

    Added code for authenticating the user

---
+++ src/main/java/com/abhishyam/learn/springsecuritylearning/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.abhishyam.learn.springsecuritylearning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/hello")
+	public String sayHello(String name) {
+		return "Hello "+name;
+	}
+	
+	
+	@GetMapping("/admin/hello")
+	public String sayAdminHello(String name) {
+		return "Hello Admin "+name;
+	}
+
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 
-INSERT into users(username,password,email,enabled) values ('siva','secret','siva@gmail.com',true);
-INSERT into users(username,password,email,enabled) values ('user','secret','user@gmail.com',true);
-INSERT into users(username,password,email,enabled) values ('admin','secret','admin@gmail.com',true);
+INSERT into users(username,password,email,enabled) values ('abhi','{noop}secret','abhi@gmail.com',true);
+INSERT into users(username,password,email,enabled) values ('user','{noop}secret','user@gmail.com',true);
+INSERT into users(username,password,email,enabled) values ('admin','{noop}secret','admin@gmail.com',true);
 INSERT into authorities values ('user','USER');
 INSERT into authorities values ('admin','ADMIN');
-INSERT into authorities values ('siva','ADMIN');
+INSERT into authorities values ('abhi','ADMIN');
 
